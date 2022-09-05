e8425b07558a11ffce369c1264bb393218ca3bcf
dakshinakasun <dakshinakasun20@gmail.com>
2022-09-05 14:06:49 +0530

    fifth commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(dakshinakasun.me, () => console.log("Server Running"));
+app.listen(react-live-app, () => console.log("Server Running"));
 // console.log(process.env.EMAIL_USER);
 // console.log(process.env.EMAIL_PASS);
 
